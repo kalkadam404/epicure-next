97c474f8577c093e40900f00bfc6d65bd44fd897
kalkadam404 <kalkadamov14@gmail.com>
2025-10-14 22:40:39 +0500

    init

---
+++ src/app/(public)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Epicure-home",
+  description: "",
+};
+
+export default function Page() {
+  return <div>Hello world</div>;
+}
